3801b1416c9f096111695921bb5295b58861cd88
MarjonaDavlyatova <marjona.davlyatova.h@gmail.com>
2022-11-17 17:03:08 +0500

    first_c

---
+++ README.md
@@ -0,0 +1,10 @@
+TO DO
+AUTH (login, logout)
+Users management app pages:
+list of the users page
+single user page
+update of the user
+delete of the user
+create of the user
+login
+logout
---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+from users import views
+
+urlpatterns = [
+    path("", views.UserListView.as_view(), name="users-list"),
+    path("create/", views.users_create, name="users-create"),
+    path("<pk>/", views.UserDetailView.as_view(), name="users-detail"),
+    path("<pk>/update/", views.users_update, name="users-update"),
+    path("<pk>/delete/", views.users_delete, name="users-delete-confirmation"),
+]
